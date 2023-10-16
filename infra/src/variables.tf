8332efabd7373932c346311f6fa80a598870a8a2
Juned Memon <junaid18183@gmail.com>
2023-10-16 19:41:33 +0530

    Added latest EKS version with KMS support

--- infra/src/variables.tf
+++ infra/src/variables.tf
@@ -1,5 +1,23 @@
-variable "region" {
-  description = "AWS region"
+variable "instance_types" {
+  description = "EKS managed node instance type"
+  type        = list(any)
+  default     = ["t3.large"]
+}
+
+variable "eks_node_groups_min_size" {
+  description = "EKS managed node "
   type        = string
-  default     = "us-east-1"
+  default     = 1
+}
+
+variable "eks_node_groups_max_size" {
+  description = "EKS managed node "
+  type        = string
+  default     = 1
+}
+
+variable "eks_node_groups_desired_size" {
+  description = "EKS managed node "
+  type        = string
+  default     = 1
 }
